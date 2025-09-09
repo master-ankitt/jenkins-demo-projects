d4e91d90d9b2df9d741c96cc97fd37e7bc2210cb
master-ankitt <ankitsinghchoudhary541@gmail.com>
2025-09-09 16:24:53 +0530

    tested files for deployment of tomcat

---
+++ tomcat-maven-deploy/src/main/java/HelloWorldApplication.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, I have successfully deployed simple hello world java maven project with Jenkins");
+    }
+}
