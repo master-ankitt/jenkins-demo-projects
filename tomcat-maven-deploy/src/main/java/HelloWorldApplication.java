cb6bf4dbda8fdc06add16add7e176c0cd35a2a1b
master-ankitt <ankitsinghchoudhary541@gmail.com>
2025-09-09 22:51:26 +0530

    tested files for deployment of tomcat

--- tomcat-maven-deploy/src/main/java/HelloWorldApplication.java
+++ tomcat-maven-deploy/src/main/java/HelloWorldApplication.java
@@ -1,7 +1,27 @@
-package com.example;
+package com.springhow.example.helloworld;
 
-public class HelloWorldApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelloWorldApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HelloWorldApplication.class);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, I have successfully deployed simple hello world java maven project with Jenkins");
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String helloWorld() {
+        return "5. please visit my website learning-ocean.com";
     }
 }
